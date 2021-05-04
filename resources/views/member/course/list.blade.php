abf11ed67f62da82a089068c3ee66661ca69353f
xiiaobudian <zhenkai.dev@gmail.com>
2021-05-04 18:05:33 +0800

    commented middleware and display message to notify inactive users to register on Mplus

--- resources/views/member/course/list.blade.php
+++ resources/views/member/course/list.blade.php
@@ -5,65 +5,70 @@
 @endsection
 
 @section('listing')
-    @php /* @var Illuminate\Pagination\LengthAwarePaginator $courses */ @endphp
+    @if (Auth::user()->isActive())
+        @php /* @var Illuminate\Pagination\LengthAwarePaginator $courses */ @endphp
 
-    @component('member.shared.listing.listing-header-component', ['module' => \App\Course::class])
-        @slot('total') {{ $courses->total() }} @endslot
-    @endcomponent
+        @component('member.shared.listing.listing-header-component', ['module' => \App\Course::class])
+            @slot('total') {{ $courses->total() }} @endslot
+        @endcomponent
 
-    @component('member.shared.listing.filter-form')
-        @slot('action') {{ route('member.course.index') }} @endslot
-        @slot('inputs')
-            @component('member.shared.listing.filter-form-group')
-                @slot('label') {{ __('course.name') }} @endslot
-                @slot('input')
-                    @component('member.shared.input.text-component')
-                        @slot('type') text @endslot
-                        @slot('name') name @endslot
-                        @slot('value') {{ request('name') }} @endslot
-                    @endcomponent
-                @endslot
-            @endcomponent
-        @endslot
-        @slot('pagination')
-            {{ $courses->links() }}
-        @endslot
-    @endcomponent
+        @component('member.shared.listing.filter-form')
+            @slot('action') {{ route('member.course.index') }} @endslot
+            @slot('inputs')
+                @component('member.shared.listing.filter-form-group')
+                    @slot('label') {{ __('course.name') }} @endslot
+                    @slot('input')
+                        @component('member.shared.input.text-component')
+                            @slot('type') text @endslot
+                            @slot('name') name @endslot
+                            @slot('value') {{ request('name') }} @endslot
+                        @endcomponent
+                    @endslot
+                @endcomponent
+            @endslot
+            @slot('pagination')
+                {{ $courses->links() }}
+            @endslot
+        @endcomponent
 
-    @if (count($courses))
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>{!! sortable('name', __('course.name')) !!}</th>
-                        <th width="10%"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach ($courses as $course)
-                    @php /* @var App\Course $course */ @endphp
-                    <tr>
-                        <td>
-                            <a href="{{ route('member.course.show', $course->getId()) }}">{{ $course->courseTranslation->getName() }}</a>
-                            <small class="font-italic d-block">{{ str_limit(strip_tags($course->courseTranslation->getDescription())) }}</small>
-                        </td>
-                        <td class="text-center">
-                            @if (!Auth::user()->can(\App\Enumeration\PolicyActionEnum::VIEW, $course))
-                                {!! view_icon_muted() !!}
-                            @else
-                                {!! view_icon_link(route('member.course.show', $course->getId())) !!}
-                            @endif
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
+        @if (count($courses))
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>{!! sortable('name', __('course.name')) !!}</th>
+                            <th width="10%"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($courses as $course)
+                        @php /* @var App\Course $course */ @endphp
+                        <tr>
+                            <td>
+                                <a href="{{ route('member.course.show', $course->getId()) }}">{{ $course->courseTranslation->getName() }}</a>
+                                <small class="font-italic d-block">{{ str_limit(strip_tags($course->courseTranslation->getDescription())) }}</small>
+                            </td>
+                            <td class="text-center">
+                                @if (!Auth::user()->can(\App\Enumeration\PolicyActionEnum::VIEW, $course))
+                                    {!! view_icon_muted() !!}
+                                @else
+                                    {!! view_icon_link(route('member.course.show', $course->getId())) !!}
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="alert alert-warning" role="alert">
+                {{ __('message.no_record') }}
+            </div>
+        @endif
+
+        {{ $courses->links() }}
     @else
-        <div class="alert alert-warning" role="alert">
-            {{ __('message.no_record') }}
-        </div>
+        Hi, {{Auth::user()->getName()}}. Please register a Mplus account using this <a href="https://registration.mplusonline.com/?ref-id=R311" target="_blank">link</a>, 
+        our team will verify your account in 3 working days. You can view our course once your account is activated.
     @endif
-
-    {{ $courses->links() }}
 @endsection
