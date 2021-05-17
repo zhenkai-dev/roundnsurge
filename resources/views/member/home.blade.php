c8f0eb63ba3717f3877f9ec1d5cc8614b45f7686
xiiaobudian <zhenkai.dev@gmail.com>
2021-05-17 12:27:33 +0800

    updated welcome message

--- resources/views/member/home.blade.php
+++ resources/views/member/home.blade.php
@@ -1,8 +1,8 @@
 @extends('member.layouts.app')
 
 @section('content')
-    <h1>Welcome!</h1>
-    <p>In order to proceed, please send us an email at <a href="mailto:roundnsurge@gmail.com">roundnsurge@gmail.com</a> when your mplus trading account is activated.</p>
+    <h1>Welcome to Round & Surge!</h1>
+    <p>In order to join our Weekly Market POV live on webinar every Monday, please send us an email at <a href="mailto:roundnsurge@gmail.com">roundnsurge@gmail.com</a> when your mplus trading account is activated.</p>
     <p>Looking forward to hear from you!</p>
 @endsection
 
