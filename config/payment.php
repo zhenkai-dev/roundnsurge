234a56ff44402d4da4a5172d961eb588c5e21076
zhenkai <zhenkai.dev@gmail.com>
2021-05-31 12:15:11 +0800

    update invoice_initial_number from 100 to 1000

--- config/payment.php
+++ config/payment.php
@@ -3,7 +3,7 @@
 return [
     'sandbox_mode' => env('PAYMENT_SANDBOX_MODE', true),
     'invoice_prefix' => date('Y'),
-    'invoice_initial_number' => 100,
+    'invoice_initial_number' => 1000,
 
     'paypal' => [
         'business_email' => env('PAYPAL_BUSINESS_EMAIL', 'kitloo_1352255941_biz@hotmail.com')
