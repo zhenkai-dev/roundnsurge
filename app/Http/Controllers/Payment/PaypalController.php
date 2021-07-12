210bc7db17b32c63c02964e453855bf51762f144
zhenkai <zhenkai.dev@gmail.com>
2021-07-12 14:02:25 +0800

    track the transaction status based on order id instead - solution provided by freelancer arpit

--- app/Http/Controllers/Payment/PaypalController.php
+++ app/Http/Controllers/Payment/PaypalController.php
@@ -49,9 +49,126 @@ class PaypalController extends Controller
     public function response(string $orderNo, Request $request)
     {
         $status = 'failed';
+
         if (!empty($orderNo)) {
+			//update order number and package id
             $order = Order::whereOrderNo($orderNo)->first();
-            if ($order) {
+
+            if ($order) {  				
+                $paidAmount = (float)$order->getAmount();
+                DB::transaction(function () use ($order, $paidAmount) {
+                    if (!$order->isPaid()) {
+                        $member = Member::find($order->getMemberId());
+        
+                        //Create invoice
+                        $invoice = new Invoice();
+                        $invoice->setPrefix(config('payment.invoice_prefix'));
+                        $invoice->setAuthKey(md5(str_random(16)));
+                        $invoice->setBillingName($order->getUsername());
+                        $invoice->setBillingEmail($order->getEmail());
+        
+                        if ($member) {
+                            $invoice->setMemberId($order->getMemberId());
+                            $invoice->setBillingContact($member->getMobile());
+                            $address = $member->address()->first();
+        
+                            if ($address) {
+                                /* @var Address $address */
+                                $invoice->setBillingAddress1($address->getAddress1());
+                                $invoice->setBillingAddress2($address->getAddress2());
+                                $invoice->setBillingPostcode($address->getPostcode());
+                                $invoice->setBillingCity($address->getCity());
+                                $invoice->setBillingState($address->getState());
+                                $invoice->setBillingCountryId($address->getCountryId());
+                            }
+                        }
+        
+                        $invoice->setCurrencyId($order->getCurrencyId());
+                        $invoice->setCurrencySymbol($order->getCurrencySymbol());
+                        $invoice->setCurrencyFormat($order->getCurrencyFormat());
+                        $invoice->setCurrencyExchangeRate($order->getCurrencyExchangeRate());
+                        $invoice->setCurrencyCode($order->getCurrencyCode());
+        
+                        $this->invoiceService->createInvoice($invoice);
+        
+                        $lastInvoice = Invoice::orderBy('id', 'desc')->lockForUpdate()->first();
+        
+                        $invoiceNo = config('payment.invoice_initial_number');
+                        /* @var \App\Invoice $lastInvoice */
+                        if ($lastInvoice != null) {
+                            if($lastInvoice == "10000000") {
+                                $invoiceNo = $invoiceNo;
+                            } else {
+                                $invoiceNo = $lastInvoice->getInvoiceNo() + 1;
+                            }
+                        } 
+        
+                        $invoice->setInvoiceNo($invoiceNo);
+                        $invoice->setAmount($order->getAmount());
+                        $invoice->setPaymentMethod(Invoice::PAY_METHOD_PAYPAL);
+                        $invoice->setPaid(true);
+                        $invoice->setPaidAmount($paidAmount);
+                        $invoice->setPaidDate($order->getCreatedAt());
+                        $invoice->setInvoiceStatus(Invoice::STATUS_PAID);
+        
+                        $invoice->save();
+        
+                        // Create invoice item
+                        if ($order->getOrderDetails()) {
+                            $invoiceItem = new InvoiceItem();
+                            $orderDetails = unserialize($order->getOrderDetails());
+        
+                            foreach ($orderDetails as $orderDetail) {
+                                $orderDetailDto = OrderDetailDto::arrayToObject($orderDetail);
+                                $invoiceItem->setItemName($orderDetailDto->getItemName());
+                                $invoiceItem->setFkid($orderDetailDto->getItemId());
+                                $invoiceItem->setModule($orderDetailDto->getItemModule());
+                                $invoiceItem->setAmount($orderDetailDto->getAmount());
+                                $invoiceItem->setQuantity($orderDetailDto->getQuantity());
+        
+                                if ($orderDetailDto->getItemModule() == Package::class) {
+                                    $package = Package::findOrFail($orderDetailDto->getItemId());
+                                    $duration = Package::getPackageDuration($package->package_type);
+        
+                                    switch ($order->getOrderType()) {
+                                        case Order::REGISTER_MEMBERSHIP:
+                                            $membership = new Membership();
+                                            $membership->setMemberId($member->getId());
+                                            $membership->setPackageId($orderDetailDto->getItemId());
+                                            $membership->setExpiryDate(Carbon::now()->addMonth($duration));
+                                            $membership->setIsActive(true);
+                                            $membership->save();
+                                            break;
+                                        case Order::UPGRADE_MEMBERSHIP:
+                                        case Order::RENEW_MEMBERSHIP:
+                                            $currentMembership = $member->membership()->first();
+                                            $membership = new Membership();
+                                            $membership->setMemberId($member->getId());
+                                            $membership->setPackageId($orderDetailDto->getItemId());
+        
+                                            if ($currentMembership->getExpiryDate() === null) {
+                                                $membership->setExpiryDate(Carbon::now()->addMonth($duration));
+                                            } else {
+                                                if ($currentMembership->isExpired()) {
+                                                    $membership->setExpiryDate(Carbon::now()->addMonth($duration));
+                                                } else {
+                                                    $membership->setExpiryDate($currentMembership->getExpiryDate()->addMonth($duration));
+                                                }
+                                            }
+        
+                                            $membership->setIsActive(true);
+                                            $membership->save();
+                                        default:
+                                    }
+                                }
+        
+                            }
+                            $invoice->invoiceItems()->save($invoiceItem);
+                        }
+                        $order->setPaid(true);
+                        $order->save();
+                    }
+                });
                 if ($order->isPaid()) {
                     $status = 'success';
                 }
@@ -68,7 +185,7 @@ class PaypalController extends Controller
      */
     public function ipn(Request $request)
     {
-
+	 //dd($request);
 // CONFIG: Enable debug mode. This means we'll log requests into 'ipn.log' in the same directory.
 // Especially useful if you encounter network errors or other intermittent problems with IPN (validation).
 // Set this to 0 once you go live or don't require logging.
@@ -405,6 +522,24 @@ class PaypalController extends Controller
                                                 $membership->save();
                                                 break;
                                             case Order::UPGRADE_MEMBERSHIP:
+                                                $currentMembership = $member->membership()->first();
+                                                $membership = new Membership();
+                                                $membership->setMemberId($member->getId());
+                                                $membership->setPackageId($orderDetailDto->getItemId());
+
+                                                if ($currentMembership->getExpiryDate() === null) {
+                                                    $membership->setExpiryDate(Carbon::now()->addMonth($duration));
+                                                } else {
+                                                    if ($currentMembership->isExpired()) {
+                                                        $membership->setExpiryDate(Carbon::now()->addMonth($duration));
+                                                    } else {
+                                                        $membership->setExpiryDate($currentMembership->getExpiryDate()->addMonth($duration));
+                                                    }
+                                                }
+
+                                                $membership->setIsActive(true);
+                                                $membership->save();
+                                                break;
                                             case Order::RENEW_MEMBERSHIP:
                                                 $currentMembership = $member->membership()->first();
                                                 $membership = new Membership();
